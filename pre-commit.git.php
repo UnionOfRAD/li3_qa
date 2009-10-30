8df676030c3cee570ab1dd6f3d2be5df4776e413
David Persson <davidpersson@gmx.de>
2009-10-30 23:05:29 +0100

    Replacing PHP_Codesniffer with phpca.
    Updating hook to support checks against multiple files.

--- pre-commit.git.php
+++ pre-commit.git.php
@@ -1,62 +1,73 @@
 #!/usr/bin/env php -q
 <?php
-/**
- * Check validity of file using PHP lint.
- */
-$phplint = function($file) {
+
+/* Configuration. Adjust as necessary. */
+
+$phpCommand = '/opt/local/bin/php';
+
+$checkEach = array();
+$checkAll = array();
+
+/* Checks. Disable/enable or add and modify. */
+
+/* Enforce Lithium coding standards using phpca. */
+$checkEach['phpca'] = function($file) use ($phpCommand) {
 	if (!file_exists($file) || !preg_match('/\.php$/', $file)) {
 		return null;
 	}
+	$phpcaCommand = escapeshellarg(__DIR__ . '/libraries/phpca/src/phpca.php');
 	$file = escapeshellarg($file);
-	exec("php -l {$file} 2> /dev/null", $output, $return);
-
-	if ($return == 0) {
-		return null;
-	}
-	return array_filter($output, function($error) { return !empty($error) && $error[0] == 'P'; });
-};
-
-/**
- * Enforce Lithium coding standards using PHP CodeSniffer.
- */
-$phpcs = function($file) {
-	if (!file_exists($file) || !preg_match('/\.php$/', $file)) {
-		return null;
-	}
-	$file = escapeshellarg($file);
-	$standard = escapeshellarg(__DIR__ . '/sniffs/Lithium');
-	exec("phpcs -n --standard={$standard} {$file}", $output, $return);
+	$standard = escapeshellarg(__DIR__ . '/extensions/phpca/Standard/lithium.ini');
+	exec("php {$phpcaCommand} -p {$phpCommand} --standard {$standard} {$file}", $output, $return);
 
 	if ($return != 0) {
-		return array_slice($output, 5, -2);
+		return array_slice($output, 9, -3);
 	}
 };
 
-/**
- * Register checks and corresponding names for display.
- */
-$checks = array(
-	'PHP Lint' => $phplint,
-	'Lithium Coding Standards' => $phpcs
-);
+/* Check validity of file using PHP lint. */
+// $checkEach['phplint'] = function($file) {
+// 	if (!file_exists($file) || !preg_match('/\.php$/', $file)) {
+// 		return null;
+// 	}
+// 	$file = escapeshellarg($file);
+// 	exec("php -l {$file} 2> /dev/null", $output, $return);
+//
+// 	if ($return == 0) {
+// 		return null;
+// 	}
+// 	return array_filter($output, function($error) { return !empty($error) && $error[0] == 'P'; });
+// };
 
-/**
- * Main execution.
- */
+/* Main execution. You should not need to change anything below. */
+
+/* Obtain a list of files with changes. */
 exec('git rev-parse --verify HEAD 2> /dev/null', $output, $return);
 $against = $return ? '4b825dc642cb6eb9a060e54bf8d69288fbee4904' : 'HEAD';
 exec("git diff-index --cached --name-only {$against}", $output);
 
+$files = $output;
+$numberFiles = count($files);
 $errorState = false;
 $hr = str_repeat('-', 80);
 
-foreach ($output as $file) {
-	echo "\nChecking file {$file}...\n";
+/* Run checks for multiple files. */
+foreach ($checkAll as $name => $check) {
+	echo "Running check `{$name}` against {$numberFiles} file(s).\n";
 
-	foreach ($checks as $name => $check) {
+	if ($errors = $check($files)) {
+		echo implode("\n", $errors) . "\n";
+		$errorState = true;
+	}
+}
+
+/* Run checks for single files. */
+foreach ($files as $file) {
+	foreach ($checkEach as $name => $check) {
+		echo "Running check `{$name}` against `{$file}`.\n";
+
 		if ($errors = $check($file)) {
-			echo "\nFailed check {$name}.\n{$hr}\n";
-			echo implode("\n", $errors) . "\n";
+			echo implode("\n", $errors) . "\n\n";
 			$errorState = true;
 		}
 	}
--- sniffs/Lithium/Sniffs/Files/LineLengthSniff.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Lithium_Sniffs_Files_LineLengthSniff extends Generic_Sniffs_Files_LineLengthSniff {
-
-	protected $lineLimit = 100;
-
-}
-
-?>
