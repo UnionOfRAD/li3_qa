b7ae0553d68b22b79b3a968ff187fe823623fd5a
David Persson <davidpersson@gmx.de>
2009-12-03 00:36:27 +0100

    New `MaximumLineLengthRule`.

---
+++ extensions/phpca/Rule/MaximumLineLengthRule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace spriebsch\PHPca\Rule;
+
+use spriebsch\PHPca\Token;
+
+/**
+ * Ensures that no line is longer than a certain amount of charcters.
+ */
+class MaximumLineLengthRule extends Rule
+{
+    /**
+     * Performs the rule check.
+     *
+     * @returns null
+     */
+    protected function doCheck()
+	{
+		$lines = explode(
+			stripcslashes($this->configuration->getLineEndings()),
+			$this->file->getSourceCode()
+		);
+		foreach ($lines as $i => $line) {
+			if (strlen($line) > $this->settings['line_length']) {
+				$token = new Token(T_ANY, $line, $i + 1, $this->settings['line_length']);
+				$this->addViolation('Maximum line length exceeded', $token);
+			}
+		}
+	}
+}
+?>
