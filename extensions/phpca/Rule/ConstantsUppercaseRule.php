031bebd2721620ddd4a0c5cc5ac0310fc6ae8c0c
David Persson <davidpersson@gmx.de>
2010-02-01 07:56:59 +0100

    Tabs to spaces for PHPca related code.

--- extensions/phpca/Rule/ConstantsUppercaseRule.php
+++ extensions/phpca/Rule/ConstantsUppercaseRule.php
@@ -8,40 +8,40 @@ use spriebsch\PHPca\Token;
  * Ensures that Constants are upper case
  */
 class ConstantsUppercaseRule extends Rule {
-	/**
+    /**
      * Performs the rule check.
      *
      * @returns null
      */
-	public function doCheck() {
-		$source = $this->file->getSourceCode();
+    public function doCheck() {
+        $source = $this->file->getSourceCode();
 
-		$namedConstants = array();
-		$definePattern  = '/' . $this->configuration->getLineEndings() . '.*?define\("(.*?)".*[^';
-		$definePattern .= $this->configuration->getLineEndings() . ']/';
+        $namedConstants = array();
+        $definePattern  = '/' . $this->configuration->getLineEndings() . '.*?define\("(.*?)".*[^';
+        $definePattern .= $this->configuration->getLineEndings() . ']/';
 
-		preg_match_all($definePattern, $source, $namedConstants);
-		$declarations = array_shift($namedConstants);
-		$namedConstants = array_shift($namedConstants);
+        preg_match_all($definePattern, $source, $namedConstants);
+        $declarations = array_shift($namedConstants);
+        $namedConstants = array_shift($namedConstants);
 
-		foreach($namedConstants as $i => $const) {
-			if($const != strtoupper($const)) {
-				$lines = array();
-				$line = preg_match_all(
-					'/.*?' . $this->configuration->getLineEndings() . '.*?/',
-					substr($source, 0, strpos($source, 'define("' . $const)),
-					$lines
-				);
+        foreach($namedConstants as $i => $const) {
+            if($const != strtoupper($const)) {
+                $lines = array();
+                $line = preg_match_all(
+                    '/.*?' . $this->configuration->getLineEndings() . '.*?/',
+                    substr($source, 0, strpos($source, 'define("' . $const)),
+                    $lines
+                );
 
-				$this->addViolation(
-					"Named Constant `" . $const . "` not upper case",
-					null,
-					$line + 1,
-					strpos($declarations[$i], $const)
-				);
-			}
-		}
-	}
+                $this->addViolation(
+                    "Named Constant `" . $const . "` not upper case",
+                    null,
+                    $line + 1,
+                    strpos($declarations[$i], $const)
+                );
+            }
+        }
+    }
 }
 
 ?>
