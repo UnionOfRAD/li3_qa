2b858a24c196eec0a4ef0dfb634025d8d576c676
gwoo <gwoohoo@gmail.com>
2010-02-23 07:57:23 -0800

    updating bootstrap for 0.7

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -61,7 +61,7 @@ Libraries::add('lithium');
  * Add the application.  You can pass a `'path'` key here if this bootstrap file is outside of
  * your main application, but generally you should not need to change any settings.
  */
-Libraries::add('app', array('bootstrap' => false));
+Libraries::add('app', array('default' => true));
 
 /**
  * Add libraries from submodules.
